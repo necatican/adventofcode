926392ef77bab5f7eba10dae50cc0da90ae613c2
Necatican Yıldırım <necaticanyildirim@gmail.com>
2020-12-01 14:11:28 +0300

    day 1 (python)

---
+++ 2020/day1/q2.py
@@ -0,0 +1,16 @@
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+input_file = open(os.path.join(dir_path, "input"), 'r') 
+lines = input_file.readlines() 
+
+numbers = set()
+for line in lines: 
+    numbers.add(int(line))
+
+for first_number in numbers:
+    desired_total = 2020 - first_number
+    for second_number in numbers:
+        third_number = desired_total - second_number
+        if third_number in numbers:
+            print(first_number * second_number * third_number )
+            exit()
