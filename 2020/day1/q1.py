f3f5ce47d80a79f76e30d05192a406ade6f7aa2e
Necatican Yıldırım <necaticanyildirim@gmail.com>
2020-12-02 11:44:02 +0300

    pep8 compliance

--- 2020/day1/q1.py
+++ 2020/day1/q1.py
@@ -1,14 +1,14 @@
 import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
-input_file = open(os.path.join(dir_path, "input"), 'r') 
-lines = input_file.readlines() 
+input_file = open(os.path.join(dir_path, "input"), 'r')
+lines = input_file.readlines()
 
 numbers = set()
-for line in lines: 
+for line in lines:
     numbers.add(int(line))
 
 for number in numbers:
     desired_num = 2020 - number
     if desired_num in numbers:
-        print(number * desired_num )
+        print(number * desired_num)
         break
--- 2020/day1/q2.py
+++ 2020/day1/q2.py
@@ -1,10 +1,10 @@
 import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
-input_file = open(os.path.join(dir_path, "input"), 'r') 
-lines = input_file.readlines() 
+input_file = open(os.path.join(dir_path, "input"), 'r')
+lines = input_file.readlines()
 
 numbers = set()
-for line in lines: 
+for line in lines:
     numbers.add(int(line))
 
 for first_number in numbers:
@@ -12,5 +12,5 @@ for first_number in numbers:
     for second_number in numbers:
         third_number = desired_total - second_number
         if third_number in numbers:
-            print(first_number * second_number * third_number )
+            print(first_number * second_number * third_number)
             exit()
